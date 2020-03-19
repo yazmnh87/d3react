e02a648ea443d955f3c8762c672d11d0a7ee5817
William Violette <violettewj@gmail.com>
2020-03-19 16:00:30 -0600

    forEach

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -4,5 +4,13 @@ const data = [20,16,12,14,18];
 export default function D3Chart(element){
   const svg = d3.select(element.current).append("svg").attr("height", 500).attr("width", 500)
 
+  data.forEach((d,i)=>{
+      svg.append("rect")
+      .attr("x", i * 100)
+      .attr("y", 50)
+      .attr("width", 50)
+      .attr("height", d)
+      .attr("fill", "blue")
+  })
   
 }
