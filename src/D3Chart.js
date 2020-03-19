e7f232af84563742218d8c317c9af0665fa18be5
William Violette <violettewj@gmail.com>
2020-03-19 16:04:11 -0600

    dataJoin

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -8,9 +8,12 @@ export default function D3Chart(element) {
     .attr("height", 500)
     .attr("width", 500);
 
-    svg.append("rect")
-        .attr("x", 50)
+   const rects = svg.selectAll("rect").data(data)
+        rects.enter()
+        .append("rect")
+        .attr("x", (d,i)=> i * 100)
         .attr("Y",50)
         .attr("width", 50)
-        .attr("height", 100)
+        .attr("height", d=> d)
+        .attr("fill", "blue")
     }
