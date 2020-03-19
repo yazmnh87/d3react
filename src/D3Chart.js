c397aebe81ffca3d0b82a3a45a6bc35ca8be7fe5
William Violette <violettewj@gmail.com>
2020-03-19 16:43:45 -0600

    externalData

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-const data = [20, 16, 12, 14, 18];
+const url = `https://udemy-react-d3.firebaseio.com/ages.json`;
 
 export default function D3Chart(element) {
   const svg = d3
@@ -8,9 +8,21 @@ export default function D3Chart(element) {
     .attr("height", 500)
     .attr("width", 500);
 
-    svg.append("rect")
-        .attr("x", 50)
-        .attr("Y",50)
-        .attr("width", 50)
-        .attr("height", 100)
-    }
+  d3.json(url).then(data => {
+    console.log(data);
+    const rects = svg.selectAll("rect").data(data);
+
+    rects
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 100)
+      .attr("y", 50)
+      .attr("width", 50)
+      .attr("height", d => d.age * 10)
+      .attr("fill", d => {
+        if (d.name === "Tony") {
+          return "red";
+        }  return "green"
+      });
+  });
+}
