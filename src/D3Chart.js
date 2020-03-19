8dc437976d525353d6a5c84986cd7f19293d40a1
William Violette <violettewj@gmail.com>
2020-03-19 15:55:46 -0600

    d3

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,8 +1,16 @@
-import * as d3 from 'd3'
-const data = [20,16,12,14,18];
+import * as d3 from "d3";
+const data = [20, 16, 12, 14, 18];
 
-export default function D3Chart(element){
-  const svg = d3.select(element.current).append("svg").attr("height", 500).attr("width", 500)
+export default function D3Chart(element) {
+  const svg = d3
+    .select(element.current)
+    .append("svg")
+    .attr("height", 500)
+    .attr("width", 500);
 
-  
-}
+    svg.append("rect")
+        .attr("x", 50)
+        .attr("Y",50)
+        .attr("width", 50)
+        .attr("height", 100)
+    }
