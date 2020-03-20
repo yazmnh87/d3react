abfc50cb2d885ce74fed039057ca610ac41052e7
William Violette <violettewj@gmail.com>
2020-03-20 12:28:24 -0600

    updated

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 const data = [20, 16, 12, 14, 18];
-const url = `https://udemy-react-d3.firebaseio.com/tallest_men.json`
-const MARGIN = {TOP: 50, RIGHT: 10, BOTTOM: 50, LEFT: 50}
+const url = `https://udemy-react-d3.firebaseio.com/tallest_men.json`;
+const MARGIN = { TOP: 50, RIGHT: 10, BOTTOM: 50, LEFT: 50 };
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
 const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 export default function D3Chart(element) {
@@ -11,53 +11,84 @@ export default function D3Chart(element) {
     .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
     .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
     .append("g")
-    .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`)
+    .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
 
-d3.json(url).then(data => {
-const max = d3.max(data, d => {
-    return d.height
-})
+  svg
+    .append("text")
+    .attr("x", WIDTH / 2)
+    .attr("y", HEIGHT + 35)
+    .attr("text-anchor", "middle")
+    .text("The worlds tallest men");
 
-const min = d3.min(data, d => d.height * .95)
+  svg
+    .append("text")
+    .attr("x", -(HEIGHT / 2))
+    .attr("y", -35)
+    .attr("text-anchor", "middle")
+    .text("Height")
+    .attr("transform", "rotate(-90)");
 
-const y = d3.scaleLinear()
-.domain([min, max])
-.range([HEIGHT,0])
+    const xAxisGroup = svg
+      .append("g")
+      .attr("transform", `translate(0, ${HEIGHT})`)
 
-const x = d3.scaleBand()
-.domain(data.map(d => d.name))
-.range([0, WIDTH])
-.padding(0.4)
+      const yAxisGroup = svg.append("g")
 
-const xAxisCall = d3.axisBottom(x)
-svg.append("g").attr("transform", `translate(0, ${HEIGHT})`).call(xAxisCall)
+  d3.json(url).then(data => {
+    d3.interval(() => {
+      update(data);
+    }, 1000);
+  });
 
-const yAxisCall = d3.axisLeft(y)
-svg.append("g").call(yAxisCall)
+  const update = (data) => {
+    const max = d3.max(data, d => {
+      return d.height;
+    });
+  
+    const min = d3.min(data, d => d.height * 0.95);
+  
+    const y = d3
+      .scaleLinear()
+      .domain([min, max])
+      .range([HEIGHT, 0]);
+  
+    const x = d3
+      .scaleBand()
+      .domain(data.map(d => d.name))
+      .range([0, WIDTH])
+      .padding(0.4);
+  
+    const xAxisCall = d3.axisBottom(x);
+    
+      xAxisGroup.call(xAxisCall);
+  
+    const yAxisCall = d3.axisLeft(y);
+    yAxisGroup.call(yAxisCall);
+  
+    //enter data
+    const rects = svg.selectAll("rect").data(data);
 
-svg.append("text")
-.attr("x", WIDTH / 2)
-.attr("y", HEIGHT + 35)
-.attr("text-anchor", "middle")
-.text("The worlds tallest men")
+    //exit
+    rects.exit().remove()
 
-svg.append("text")
-.attr("x", -(HEIGHT /2))
-.attr("y", -35)
-.attr("text-anchor", "middle")
-.text("Height")
-.attr("transform","rotate(-90)")
+    //update
+    rects
+    .attr("x", (d, i) => x(d.name))
+    .attr("y", d => y(d.height))
+    .attr("width", x.bandwidth)
+    .attr("height", d => HEIGHT - y(d.height))
 
-   const rects = svg.selectAll("rect")
-        .data(data)
+    //enter
+    rects
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => x(d.name))
+      .attr("y", d => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", d => HEIGHT - y(d.height))
+      .attr("fill", "blue");
+  };
 
-        rects.enter()
-        .append("rect")
-            .attr("x", (d,i)=> x(d.name))
-            .attr("y", d => y(d.height))
-            .attr("width", x.bandwidth)
-            .attr("height", d => HEIGHT - y(d.height))
-            .attr("fill", "blue")
-})
+}
 
-    }
+
